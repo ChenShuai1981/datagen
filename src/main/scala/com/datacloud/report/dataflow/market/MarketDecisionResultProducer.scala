6e3e0a4444e77b9866e637c508477e170c7f0dcc
Chen Shuai <chenshuai1@niwodai.net>
2018-05-11 13:55:05 +0800

    test

--- src/main/scala/com/datacloud/report/dataflow/market/MarketDecisionResultProducer.scala
+++ src/main/scala/com/datacloud/report/dataflow/market/MarketDecisionResultProducer.scala
@@ -8,34 +8,36 @@ import org.apache.kafka.common.serialization.StringSerializer
 
 object MarketDecisionResultProducer {
 
-  val topic = "sit_MARKET_DECISION_RESULT"
+  def main(args: Array[String]) = {
+    val topic = "dev_MARKET_DECISION_RESULT"
 
-  val props = new Properties
+    val props = new Properties
 
-  import io.confluent.kafka.serializers.KafkaAvroSerializer
-  import org.apache.kafka.clients.producer.ProducerConfig
+    import io.confluent.kafka.serializers.KafkaAvroSerializer
+    import org.apache.kafka.clients.producer.ProducerConfig
 
-  props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
-  props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
-  props.put(ProducerConfig.ACKS_CONFIG, "all")
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+    props.put(ProducerConfig.ACKS_CONFIG, "all")
 
-//  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    //  props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
 
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "ambari-agent4.sit.geerong.com:9092")
     props.put("schema.registry.url", "http://ambari-agent4.sit.geerong.com:8081")
 
-  val producer: Producer[String, String] = new KafkaProducer[String, String](props)
-  val interval = 50
-  val marketDecisionResults = MarketDecisionResultCreator.getMarketDecisionResults
-  marketDecisionResults.foreach(marketDecisionResult => {
-    val jsonString = JsonUtil.toJson(marketDecisionResult)
-    producer.send(
-      new ProducerRecord[String, String](topic, null, marketDecisionResult.transactionTime, marketDecisionResult.marketProcessId, jsonString))
-    println(jsonString)
-    Thread.sleep(interval)
-  })
+    val producer: Producer[String, String] = new KafkaProducer[String, String](props)
+    val interval = 50
+    val marketDecisionResults = MarketDecisionResultCreator.getMarketDecisionResults
+    marketDecisionResults.foreach(marketDecisionResult => {
+      val jsonString = JsonUtil.toJson(marketDecisionResult)
+      producer.send(
+        new ProducerRecord[String, String](topic, null, marketDecisionResult.transactionTime, marketDecisionResult.marketProcessId, jsonString))
+      println(jsonString)
+      Thread.sleep(interval)
+    })
 
-  producer.flush()
-  producer.close()
+    producer.flush()
+    producer.close()
+  }
 
 }
